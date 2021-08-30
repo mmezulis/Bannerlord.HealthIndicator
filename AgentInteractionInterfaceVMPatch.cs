80d7f33b5f267c8834cd2406d5423b44acddeedb
m_mezulis <martins.mezulis1@gmail.com>
2021-08-30 21:53:01 +0300

    Add project files.

---
+++ AgentInteractionInterfaceVMPatch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.ViewModelCollection.Multiplayer;
+using TaleWorlds.Core;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Bannerlord.HealthIndicator.Patches
+{
+    public class AgentInteractionInterfaceVMPatch
+    {
+        [AllowNull]
+        static AgentInteractionInterfaceVM agentVm;
+
+        [HarmonyPatch(typeof(AgentInteractionInterfaceVM), "SetAgent")]
+        public class SetAgentPatch{
+            static void Postfix(Agent focusedAgent, AgentInteractionInterfaceVM __instance)
+            {
+                __instance.PrimaryInteractionMessage = __instance.PrimaryInteractionMessage + $" ({focusedAgent.Health}/{focusedAgent.HealthLimit})";
+                agentVm = __instance;
+                focusedAgent.OnAgentHealthChanged += HandleOnAgentHealthChanged;
+            }
+        }
+
+        [HarmonyPatch(typeof(AgentInteractionInterfaceVM), "OnFocusLost")]
+        public class OnFocusLostPatch
+        {
+            static void Postfix(Agent agent)
+            {
+                agent.OnAgentHealthChanged -= HandleOnAgentHealthChanged;
+            }
+        }
+
+        private static void HandleOnAgentHealthChanged(Agent agent, float oldHealth, float newHealth)
+        {
+            agentVm.PrimaryInteractionMessage =  $"{agent.Name} ({newHealth}/{agent.HealthLimit})";
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,24 @@
+﻿using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+using System;
+using TaleWorlds.Core;
+
+namespace Bannerlord.HealthIndicator
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            var harmony = new Harmony("Bannerlord.HealthIndicator");
+            try
+            {
+                harmony.PatchAll();
+            }
+            catch (Exception e)
+            {
+                InformationManager.DisplayMessage(new InformationMessage(e.ToString()));
+            }
+        }
+    }
+}
